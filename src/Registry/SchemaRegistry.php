cbf590ecb99703e1a8ac9dac7d1009e678f4c16d
Nick Chiu <coding.nikazu@gmail.com>
2022-11-23 19:59:27 +0100

    fix cs

--- src/Registry/SchemaRegistry.php
+++ src/Registry/SchemaRegistry.php
@@ -135,11 +135,11 @@ final class SchemaRegistry implements SchemaRegistryInterface
         }
 
         $schemaData = json_decode($fileContent, true, JSON_THROW_ON_ERROR);
-	$namespace = '';
+        $namespace = '';
 
-	if (true === isset($schemaData['namespace'])) {
-	    $namespace = $schemaData['namespace'];
-	}
+        if (true === isset($schemaData['namespace'])) {
+            $namespace = $schemaData['namespace'];
+        }
 
         if (null === $schemaData) {
             throw new SchemaRegistryException(sprintf(SchemaRegistryException::FILE_INVALID, $fileName));
