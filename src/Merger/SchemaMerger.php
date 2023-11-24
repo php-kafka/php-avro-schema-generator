da2d170dbc00060edc71f478eb296da462e69208
Nick Chiu <coding.nikazu@gmail.com>
2023-11-24 11:01:48 +0100

    fix embedded root schema

--- src/Merger/SchemaMerger.php
+++ src/Merger/SchemaMerger.php
@@ -118,8 +118,17 @@ final class SchemaMerger implements SchemaMergerInterface
     ): string {
         $idString = '"' . $schemaId . '"';
         $pos = (int) strpos($rootDefinition, $idString);
+        $embeddedDefinitionWithoutLevel = $this->removeSchemaLevel($embeddedDefinition);
 
-        return substr_replace($rootDefinition, $embeddedDefinition, $pos, strlen($idString));
+        return substr_replace($rootDefinition, $embeddedDefinitionWithoutLevel, $pos, strlen($idString));
+    }
+
+    private function removeSchemaLevel(string $embeddedDefinition): string
+    {
+        $arraySchema = json_decode($embeddedDefinition, true);
+        unset($arraySchema['schema_level']);
+
+        return json_encode($arraySchema) ?? '';
     }
 
     /**
